1265cd6eb85f0ba8cb76c07a271c0abc3e7a2f00
ahanuszczak <hanyhusajn@gmail.com>
2023-03-24 13:42:18 +0100

    initial commit

---
+++ app/src/main/java/com/hanuszczak/eggtimernotifications/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hanuszczak.eggtimernotifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hanuszczak.eggtimernotifications.ui.EggTimerFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, EggTimerFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
