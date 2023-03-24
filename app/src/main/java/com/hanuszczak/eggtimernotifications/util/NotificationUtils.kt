198f0c6cc3e3b21b0e88647ffee0d61885d8ee0d
ahanuszczak <hanyhusajn@gmail.com>
2023-03-24 21:51:22 +0100

    Snooze Action | SnoozeReceiver

--- app/src/main/java/com/hanuszczak/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/hanuszczak/eggtimernotifications/util/NotificationUtils.kt
@@ -24,6 +24,7 @@ import android.graphics.BitmapFactory
 import androidx.core.app.NotificationCompat
 import com.hanuszczak.eggtimernotifications.MainActivity
 import com.hanuszczak.eggtimernotifications.R
+import com.hanuszczak.eggtimernotifications.receiver.SnoozeReceiver
 
 // Notification ID.
 private val NOTIFICATION_ID = 0
@@ -57,7 +58,13 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .bigPicture(eggImage)
         .bigLargeIcon(null)
     // TODO: Step 2.2 add snooze action
-
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    val snoozePendingIntent: PendingIntent = PendingIntent.getBroadcast(
+        applicationContext,
+        REQUEST_CODE,
+        snoozeIntent,
+        PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+    )
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
     val builder = NotificationCompat.Builder(
         applicationContext,
@@ -78,7 +85,11 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setStyle(bigPicStyle)
         .setLargeIcon(eggImage)
         // TODO: Step 2.3 add snooze action
-
+        .addAction(
+            R.drawable.egg_icon,
+            applicationContext.getString(R.string.snooze),
+            snoozePendingIntent
+        )
         // TODO: Step 2.5 set priority
 
     // TODO: Step 1.4 call notify
